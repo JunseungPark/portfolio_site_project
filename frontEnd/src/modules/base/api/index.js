c8f598787486e7daa3043bc4ec1b96196d87a50f
JunseungPark <dpfwl8745@gmail.com>
2022-08-26 19:03:34 +0900

    api인스턴스

--- frontEnd/src/modules/base/api/index.js
+++ frontEnd/src/modules/base/api/index.js
@@ -1,33 +1,48 @@
-import Axios from 'axios'
+import axios, { AxiosRequestConfig } from "axios";
 
-const axios = Axios.create({
-    baseURL: 'http://localhost:8080/',
-  });
-  
-function getGoogleFonts() {
-	return axios.get(`https://www.googleapis.com/webfonts/v1/webfonts?sort=alpha&key=${process.env.VUE_APP_GOOGLE_API_KEY}`)
-	.then(({ data }) => {
-			// get only the fonts that have variants lenghts bigger then 3
-			const googleFonts = data.items
-			.filter(item => item.variants.length >= 3)
-			.map(item => {
-			const filteredVariants = item.variants.filter(variant => variant.indexOf('italic') === -1)
-			// Create a copy of the item (rather than modifying the original data),
-			// but substitute the variants array with the filtered variants
-			return { ...item, variants: filteredVariants }
-			})
-			.filter(item => {
-			const regularPosition = item.variants.indexOf('regular')
-			return regularPosition > 0 && 
-					regularPosition < item.variants.length - 1
-			})
-			return googleFonts;
-	})
-	.catch((err) => {
-			if (!axios.isCancel(err)) {
-			console.log('Error ', err.message)
-			}
-	});
+export default class mainService {
+  // 인스턴스 생성 및 정의
+  constructor() {
+    let axiosInstance = axios.create({
+      baseURL: `http://localhost:42088/api`
+    })
+  }
+
+  // axiosCall에 대한 결과값
+  async axiosCall(AxiosRequestConfig) {
+    try {
+      const { data } = await this.axiosInstance.request(config);
+      return data;
+    } catch (error: any) {
+      return error;
+    }
+  }
+
+  // api 호출 함수정의 config
+  // 카트리스트
+  async getCart(userId: string) {
+    return this.axiosCall<upso.CartListResponse>({ method: "get", url:`/Cart/GetCart?userId=${userId}&currentPage=1&countPerPage=10`});
+  }
+  // 카트추가
+  async addCart(cartRequest: upso.CartRequest) {
+    return this.axiosCall<any>({ method: "post", url:`/Cart/AddCart`, data: cartRequest });
+  }
+  // 카트수정
+  async modifyCart(cartModifyRequest: upso.CartModifyRequest) {
+    return this.axiosCall<any>({ method: "put", url:`/Cart/ModifyCart`, data: cartModifyRequest });
+  }
+  // 카트삭제
+  async deleteCart(deleteCartRequest: upso.CartDeleteRequest) {
+    return this.axiosCall<any>({ method: "delete", url:`/Cart/DeleteCartItem`, data: deleteCartRequest })
+  }
+  // 오더히스토리 추가
+  async setOrderHistory(orderRequest: upso.OrderRequest) {
+    return this.axiosCall<any>({ method: "post", url:`/Order/AddOrder`, data: orderRequest})
+  }
+  // 오더히스토리 가져오기
+  async getOrderHistory(userId: string, currentPage: number, countPerPage: number) {
+    return this.axiosCall<upso.OrderHistoryResponse>({ method: "get", url:`/Order/GetOrderList?userId=${userId}&currentPage=${currentPage}&countPerPage=${countPerPage}`})
+  }
 }
 
-  export { getGoogleFonts };
+export const upsoAPI = new UpsoAPIService();
